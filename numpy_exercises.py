650baf776aca8628cae72fda836dfcf5cc995eba
Yvette Ibarra <yvette.ibarra01@gmail.com>
2022-09-23 10:01:44 -0500

    add part 1 of exercise 8

--- numpy_exercises.py
+++ numpy_exercises.py
@@ -42,3 +42,47 @@ np.std(np.square(a))
 
 # 7 Calculate the z-score for each data point. Recall that the z-score is given by:
 (a - a.mean())/a.std()
+
+
+## Setup 1
+a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+# Use python's built in functionality/operators to determine the following:
+
+a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+a = np.array(a)
+
+
+# Exercise 1 - Make a variable called sum_of_a to hold the sum of all the numbers in above list
+sum_of_a = sum(a)
+print(sum_of_a)
+
+# Exercise 2 - Make a variable named min_of_a to hold the minimum of all the numbers in the above list
+min_of_a= min(a)
+print (min_of_a)
+
+# Exercise 3 - Make a variable named max_of_a to hold the max number of all the numbers in the above list
+max_of_a = max(a)
+print (max_of_a)
+
+# Exercise 4 - Make a variable named mean_of_a to hold the average of all the numbers in the above list
+mean_of_a = (a).mean()
+print(mean_of_a)
+
+# Exercise 5 - Make a variable named product_of_a to hold the product of multiplying all the numbers in 
+# the above list together
+product_of_a = np.prod((a))
+print(product_of_a)
+
+# Exercise 6 - Make a variable named squares_of_a.
+# It should hold each number in a squared like [1, 4, 9, 16, 25...]
+squared_of_a = a**2
+print (squared_of_a)
+
+# Exercise 7 - Make a variable named odds_in_a. It should hold only the odd numbers
+odds_in_a = a[a% 2!= 0]
+print(odds_in_a)
+
+# Exercise 8 - Make a variable named evens_in_a. It should hold only the evens.
+evens_in_a = a[a% 2== 0]
+print(evens_in_a)
